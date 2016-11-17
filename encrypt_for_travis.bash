e4c605a9ecff7626dd3337edcd9d4ac9d9514f3c
Tomasz Słodkowicz <slodki@users.noreply.github.com>
2016-11-17 22:15:46 +0100

    Detect wrong parameter, add some comments

--- encrypt_for_travis.bash
+++ encrypt_for_travis.bash
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 openssl rsautl -encrypt -pubin -inkey <(
+  # export HTTPS_PROXY='10.1.0.7:8080'   # uncomment to set proxy for curl
+  [[ "$1" != *?/?* ]] && set -- # reset wrong parameter not suitable for API call
   re='"key":"([^"]+)"'
-  [[ "$(curl --silent "https://api.travis-ci.org/repos/$1/key")" =~ $re ]]
-  echo -en "${BASH_REMATCH[1]:?Travis pub RSA key not found for repo \"$1\"}"
-) | openssl base64 -A
+  [[ "$(curl --silent "https://api.travis-ci.org/repos/${1:?Need user/repo as parameter}/key")" =~ $re ]]
+  key="${BASH_REMATCH[1]:?Travis pub RSA key not found for repo $1}"
+  echo -e "${key//RSA PUBLIC KEY/PUBLIC KEY}" # | tee /dev/stderr   # uncomment to send key copy to stderr
+) | openssl base64 -A # encode after encryption without splitting into multiple lines
+echo # add newline at the end of encoded text
